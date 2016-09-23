989f3891f996c9f1664b657553966d0e4a0d93f4
kdkoerts <kdkoerts@gmail.com>
2016-09-23 15:50:05 +0200

    Ball start

---
+++ Pong/Pong/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    class Ball : GameObject
+    {
+        public Vector2 Position;
+        public Vector2 Speed;
+        public Texture2D sprite;
+
+        public void Update()
+        {
+            Position = Position + Speed;
+            // Collision nog toevoegen
+
+
+        }
+    }
+}
