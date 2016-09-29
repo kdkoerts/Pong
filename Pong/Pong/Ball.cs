a672fa8c8c4edfd8ad4a5fb09e8f6678ed7c6498
Arvid <arvid.kater@gmail.com>
2016-09-29 20:15:03 +0200

    Angle change is implemented but not working correctly

--- Pong/Pong/Ball.cs
+++ Pong/Pong/Ball.cs
@@ -53,7 +53,7 @@ namespace Pong
             }
             //#########################################################################
             //Check For platform Collision
-            //Get easy acces to nececary variables
+            //Get easy acces to nececary variables, could this work with a reference or pointer etc.?
             Vector2 PRP = Game.PlatformRight.Position;
             Vector2 PLP = Game.PlatformLeft.Position;
             int PRH = Game.PlatformRight.sprite.Height;
@@ -68,13 +68,42 @@ namespace Pong
             if ((  Position.Y < PLP.Y + PLH && Position.Y + sprite.Height > PLP.Y && Position.X < PLP.X + PLW  )||
                 (  Position.Y < PRP.Y + PRH && Position.Y + sprite.Height > PRP.Y && Position.X + sprite.Width > PRP.X  ))
             {
-                //Bounce
-                Velocity.X = -Velocity.X;
+                Vector2 CPP;
+                int CPW;
+                int CPH;
+                float speed = (float)Math.Sqrt(Math.Pow(Velocity.X, 2) + Math.Pow(Velocity.Y, 2));
+                if (Position.X > Game.SchreenWith / 2) //wich side are we on?
+                {
+                    //Right Side
+                    CPP = PRP;
+                    CPW = PRW;
+                    CPH = PRH;
+                }
+                else
+                {
+                    //Left Side
+                    CPP = PLP;
+                    CPW = PLW;
+                    CPH = PLH;
+                    speed = speed * -1;
+                }
+                //Find Relative position
+                Vector2 RelativePos = new Vector2();
+                RelativePos = new Vector2((Position.X + sprite.Width / 2) - (CPP.X + CPW / 2), (Position.Y + sprite.Height / 2) - (CPP.Y + CPH / 2));
+                float MaxRelativePos = CPH / 2 + sprite.Height;
+                //float entryAngle = 
+                float exitAngle = (RelativePos.Y / MaxRelativePos) * 90; //Could cause problems in extreme cases (platform edges) TO BE TESTED
+
+                Velocity.X = speed * (float)Math.Cos(exitAngle);
+                Velocity.Y = speed * (float)Math.Cos(exitAngle);                   
+
+
                 Velocity = Velocity * VelocityModifier; //add speed on collision
+                if (Velocity.X == 0 && Velocity.Y == 0) Spawn();
             }
 
             /*
-            //Collision Right
+            //Collision Right: Merged into previous if(){}
             if (Position.Y < PRP.Y + PRH && Position.Y + sprite.Height > PRP.Y && Position.X + sprite.Width > PRP.X)
             {
                 //Bounce
